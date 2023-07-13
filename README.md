487f0f7ba12f547716c92db3368c937083b14464
EvgenyDavydenko <frontend82@mail.ru>
2023-07-13 12:53:10 +0300

    make model and migration for post table

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Пример создания блога с использованием фреймворка Laravel
 
 1.  Создал проект на базе фреймворка Laravel: `composer create-project laravel/laravel:^7.0`
+2.  Создал модель и миграцию для таблицы постов блога: `php artisan make:model Models/Post --migration; php artisan migrate`
---
+++ database/migrations/2023_07_13_094841_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+            $table->string('title');
+            $table->string('anons');
+            $table->string('img')->nullable();
+            $table->text('content');
+            $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
